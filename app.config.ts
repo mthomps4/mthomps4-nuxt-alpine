ca09ac91f4b61bcc528a0499bcc849fb3b80d0e9
Matthew Thompson <matthew.thompson.a@gmail.com>
2023-10-10 20:12:46 -0400

    feat: Title/Description

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Matt Thompson',
+    description: 'Thoughts from an engineer without coffee...',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
